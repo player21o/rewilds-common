a94e2a18d429a550f549d0755a86cf7283f292ac
player21o <196048914+player21o@users.noreply.github.com>
2025-04-15 15:40:16 +0200

    created converter reverter pairs

--- constructors.ts
+++ constructors.ts
@@ -1,20 +1,27 @@
 import { int, string } from "./types";
 
 export const constructors = {
+  //entities
   Citizen: {
     sid: int,
     x: int,
     y: int,
     name: string,
   },
+
+  //packets
+  pointer: {
+    x: int,
+    y: int,
+  },
 } as const;
 
 export type Constructors = {
-  [P in keyof typeof constructors]: {
-    [F in keyof (typeof constructors)[P]]: (typeof constructors)[P][F] extends (
-      ...args: any
-    ) => any
-      ? ReturnType<(typeof constructors)[P][F]>
+  [K in keyof typeof constructors]: {
+    [P in keyof (typeof constructors)[K]]: (typeof constructors)[K][P] extends { // Extract the first function's input type
+      0: (val: infer T) => any;
+    }
+      ? T
       : never;
   };
 };
--- types.ts
+++ types.ts
@@ -1,3 +1,12 @@
-export const int = (val: number): number => val | 0;
+//first function *converts* a property into a networked format
+//second function *reverts* a networked format into a property
 
-export const string = (val: string): string => val.toString();
+export const int = [
+  (val: number): number => val | 0,
+  (val: number): number => val,
+] as const;
+
+export const string = [
+  (val: string): string => val.toString(),
+  (val: string): string => val,
+] as const;

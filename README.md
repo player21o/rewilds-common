d2054db8aea9fa1cb5771a694f037bf8d96efa9a
player21o <196048914+player21o@users.noreply.github.com>
2025-04-15 16:18:11 +0200

    created readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# rewilds-common
+
+A shared repo for rewilds which helps to connect the client and server.
--- constructors.ts
+++ constructors.ts
@@ -18,7 +18,8 @@ export const constructors = {
 
 export type Constructors = {
   [K in keyof typeof constructors]: {
-    [P in keyof (typeof constructors)[K]]: (typeof constructors)[K][P] extends { // Extract the first function's input type
+    [P in keyof (typeof constructors)[K]]: (typeof constructors)[K][P] extends {
+      // Extract the first function's input type
       0: (val: infer T) => any;
     }
       ? T
